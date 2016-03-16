28e3d9e3abb4d7eb2b749a750cfa9e10edf95a52
Darwin Ding <dingdarwin@gmail.com>
2016-03-16 10:52:25 -0400

    cleaned up that file, also added comments and removed Wall

--- linkedlist/msft_ll_circular_insert.cpp
+++ linkedlist/msft_ll_circular_insert.cpp
@@ -1,6 +1,9 @@
 /*
   given a circular linked list, sorted in ascending order, insert a new node
 
+  the test-cases i wrote require a new version of g++ (use -std=c++11 as one of the
+  compiler options)
+
   solution: given the entry node, create a new one right after representing the new node and then float the node right until it reaches the right spot (with checking for infinite).
 
   new node x is in the right position if:
@@ -15,13 +18,15 @@
 #include <initializer_list>
 #include <vector>
 
-struct Node {
+struct Node 
+{
   int value;
   Node* next;
 
-  Node() {}
-  Node(int _value) : value(_value) {}
-  Node(int values[], int size) {
+  Node() : value(0), next(NULL) {}
+  Node(int _value) : value(_value), next(NULL) {}
+  Node(int values[], int size) : value(0), next(NULL)
+  {
     Node* tmp = this;
     for (int i = 0; i < size; i++) {
       tmp->value = values[i];
@@ -32,9 +37,11 @@ struct Node {
     }
     tmp->next = this;
   }
-  Node(std::vector<int> values) {
+  
+  Node(std::vector<int> values)  : value(0), next(NULL)
+  {
     Node* tmp = this;
-    for (int i = 0; i < values.size(); i++) {
+    for (unsigned int i = 0; i < values.size(); i++) {
       tmp->value = values[i];
       if (i != values.size()-1) {
 	tmp->next = new Node();
@@ -49,7 +56,8 @@ struct Node {
   void delete_list();
 };
 
-void Node::print_list() const{
+void Node::print_list() const
+{
   std::cout << "output: " << value << ", ";
   Node* tmp = next;
   while (tmp != this) {
@@ -59,20 +67,19 @@ void Node::print_list() const{
   std::cout << std::endl;
 }
 
-void Node::delete_list() {
-  Node* tmp = next;
-  std::vector<Node*> death_row;
-  death_row.push_back(this);
-  while (tmp != this) {
-    death_row.push_back(tmp);
-    tmp = tmp->next;
-  }
-  for (int i = 0; i < death_row.size(); i++) {
-    delete death_row[i];
+void Node::delete_list() 
+{
+  Node* tmp = this;
+  Node* this_ptr = this;
+  while (tmp != this_ptr) {
+    Node* next = tmp->next;
+    delete tmp;
+    tmp = next;
   }
 }
 
-Node* Node::insert_node(int new_v) {
+Node* Node::insert_node(int new_v) 
+{
   // create new node to store current value
   Node* new_node = new Node(new_v);
   new_node->next = next;
@@ -100,7 +107,8 @@ Node* Node::insert_node(int new_v) {
   return tmp;
 }
 
-struct Test_case {
+struct Test_case 
+{
   std::vector<int> input;
   int insert;
   std::vector<int> output;
@@ -110,27 +118,22 @@ struct Test_case {
 	    std::initializer_list<int> _output, std::string _id) : input(_input), insert(_insert), output(_output), id(_id) {}
 };
 
-int main() {
+int main() 
+{
   std::vector<Test_case> cases = {Test_case({5,5,5}, 6, {5,5,5,6},std::string("all same test")),
 		       Test_case({2,4,6,8,10}, 6, {2,4,6,6,8,10}, std::string("normal add")),
 		       Test_case({2,4,6,8,10}, 1, {1,2,4,6,8,10}, std::string("wrap around add + min")),
 		       Test_case({2,4,6,8,10}, 12, {2,4,6,8,10,12}, std::string("normal add + max"))};
-  for (int i = 0; i < cases.size(); i++) {
+  for (unsigned int i = 0; i < cases.size(); i++) {
     std::cout << "doing test: " << cases[i].id << std::endl;
     Node* tmp = new Node(cases[i].input);
     tmp->insert_node(cases[i].insert);
     tmp->print_list();
     std::cout << "expected output: ";
-    for (int x = 0; x < cases[i].output.size(); x++)
+    for (unsigned int x = 0; x < cases[i].output.size(); x++)
       std::cout << (cases[i].output)[x] << ", ";
     std::cout << std::endl;
     tmp->delete_list();
   }
-
-  //Node* tmp = new Node(tmp_list,5);
-  //Node* tmp_1 = tmp->insert_node(6);
-  //  std::cout << tmp_1->value << std::endl;
-  //tmp->print_list();
-  //tmp->delete_list();
 }
 
