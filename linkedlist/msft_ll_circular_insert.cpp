658057d524880a5d5760b400ad985aa5e50d3384
Darwin Ding <dingdarwin@gmail.com>
2016-03-16 00:09:18 -0400

    added pretty cool test case struct!

--- linkedlist/msft_ll_circular_insert.cpp
+++ linkedlist/msft_ll_circular_insert.cpp
@@ -1,10 +1,7 @@
 /*
-  MSFT question
-
   given a circular linked list, sorted in ascending order, insert a new node
 
-  solution: given the entry node, create a new one right after (as the old entry) and then float the node 
-  left/right accordingly.
+  solution: given the entry node, create a new one right after representing the new node and then float the node right until it reaches the right spot (with checking for infinite).
 
   new node x is in the right position if:
   a <= x <= b
@@ -14,6 +11,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <initializer_list>
 #include <vector>
 
 struct Node {
@@ -33,19 +32,31 @@ struct Node {
     }
     tmp->next = this;
   }
+  Node(std::vector<int> values) {
+    Node* tmp = this;
+    for (int i = 0; i < values.size(); i++) {
+      tmp->value = values[i];
+      if (i != values.size()-1) {
+	tmp->next = new Node();
+	tmp = tmp->next;
+      }
+    }
+    tmp->next = this;
+  }
 
-  void print_list();
+  void print_list() const;
   Node* insert_node(int value);
   void delete_list();
 };
 
-void Node::print_list() {
-  std::cout << "starting print" << std::endl << "this: " << value << std::endl;
+void Node::print_list() const{
+  std::cout << "output: " << value << ", ";
   Node* tmp = next;
   while (tmp != this) {
-    std::cout << tmp->value << std::endl;
+    std::cout << tmp->value << ",";
     tmp = tmp->next;
   }
+  std::cout << std::endl;
 }
 
 void Node::delete_list() {
@@ -71,11 +82,12 @@ Node* Node::insert_node(int new_v) {
   Node* tmp = new_node;
   bool not_first = false;
   while (tmp != next || !not_first) { // breaks if we came full circle 
-    if (tmp->value >= prev_tmp->value && // check if fits or new max
+    if ((tmp->value >= prev_tmp->value) && // check if fits or new max
 	(tmp->value <= tmp->next->value ||
 	 (tmp->value > tmp->next->value && prev_tmp->value > tmp->next->value)))
       return tmp;
-    else if (prev_tmp->value > tmp->value && tmp->value <= tmp->next->value)
+    else if (prev_tmp->value > tmp->value && tmp->value <= tmp->next->value
+	     && prev_tmp->value > tmp->next->value)
       return tmp; // for new min
     // if it doesn't fit, swap
     int tmp_int = tmp->value;
@@ -88,12 +100,37 @@ Node* Node::insert_node(int new_v) {
   return tmp;
 }
 
+struct Test_case {
+  std::vector<int> input;
+  int insert;
+  std::vector<int> output;
+  std::string id;
+
+  Test_case(std::initializer_list<int> _input, int _insert, 
+	    std::initializer_list<int> _output, std::string _id) : input(_input), insert(_insert), output(_output), id(_id) {}
+};
+
 int main() {
-  int tmp_list[] = {5,5,5,5,5};
-  Node* tmp = new Node(tmp_list,5);
-  Node* tmp_1 = tmp->insert_node(6);
+  std::vector<Test_case> cases = {Test_case({5,5,5}, 6, {5,5,5,6},std::string("all same test")),
+		       Test_case({2,4,6,8,10}, 6, {2,4,6,6,8,10}, std::string("normal add")),
+		       Test_case({2,4,6,8,10}, 1, {1,2,4,6,8,10}, std::string("wrap around add + min")),
+		       Test_case({2,4,6,8,10}, 12, {2,4,6,8,10,12}, std::string("normal add + max"))};
+  for (int i = 0; i < cases.size(); i++) {
+    std::cout << "doing test: " << cases[i].id << std::endl;
+    Node* tmp = new Node(cases[i].input);
+    tmp->insert_node(cases[i].insert);
+    tmp->print_list();
+    std::cout << "expected output: ";
+    for (int x = 0; x < cases[i].output.size(); x++)
+      std::cout << (cases[i].output)[x] << ", ";
+    std::cout << std::endl;
+    tmp->delete_list();
+  }
+
+  //Node* tmp = new Node(tmp_list,5);
+  //Node* tmp_1 = tmp->insert_node(6);
   //  std::cout << tmp_1->value << std::endl;
-  tmp->print_list();
-  tmp->delete_list();
+  //tmp->print_list();
+  //tmp->delete_list();
 }
 
